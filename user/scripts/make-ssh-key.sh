3d70b1c4c41e003f7dce2a7e7b3fcdb1f08499db
James Arcus <jimbo@ucc.asn.au>
2021-10-13 11:13:59 +0800

    Rename default SSH key to just 'id'

--- user/scripts/make-ssh-key.sh
+++ user/scripts/make-ssh-key.sh
@@ -1,6 +1,14 @@
 pref_keytype="ed25519"
 
-if [[ ! -e ~/.ssh/id_${pref_keytype} ]]; then
-    echo "Generating new SSH key..."
-    ssh-keygen -t ${pref_keytype}
+if [[ ! -e ~/.ssh/id ]]; then
+    if [[ ! -e ~/.ssh/id_${pref_keytype} ]]; then
+        echo "Generating new SSH key..."
+        ssh-keygen -t ${pref_keytype} -f ~/.ssh/id
+    else
+        echo "Moving existing SSH key to ~/.ssh/id"
+        mv ~/.ssh/id_${pref_keytype} ~/.ssh/id
+        if [[ -e ~/.ssh/id_${pref_keytype}.pub ]]; then
+            mv ~/.ssh/id_${pref_keytype}.pub ~/.ssh/id.pub
+        fi
+    fi
 fi
